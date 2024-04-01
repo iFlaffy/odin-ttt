56cbae98283836c8726e1beb090dde357ceb1e0e
Reid <50605126+iFlaffy@users.noreply.github.com>
2024-04-01 14:44:49 +1300

    Yes

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const Gameboard = (function () {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+})();
+
+function createPlayer (name) {
+    return {name};
+}
+
+const reid = createPlayer("Reid");
+
+console.log(reid.name);
